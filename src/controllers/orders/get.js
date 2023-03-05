1c5fca8a56fb2755ec0d9976cd5657e37693aa73
Андрій Шевельов <ashevelevmti@gmail.com>
2023-03-05 22:12:13 +0200

    add

--- src/controllers/orders/get.js
+++ src/controllers/orders/get.js
@@ -3,7 +3,19 @@
 const Orders = require("../../db/schema/orders");
 
 const getOrder = async function (req, res, next) {
-  const data = await Orders.find(req?.query).populate("user");
+  const date = undefined;
+  const status = req?.query.status ? req?.query.status : undefined;
+  const DATE = date?.split("-")?.map((e) => Number(e));
+  const searchDate = DATE
+    ? {
+        createdAt: {
+          $gte: new Date(DATE[0], DATE[1] - 1, DATE[2]),
+          $lte: new Date(DATE[0], DATE[1] - 1, DATE[2] + 1),
+        },
+      }
+    : undefined;
+
+  const data = await Orders.find({ ...status, ...searchDate }).populate("user");
 
   res.status(200).json(data);
 };
--- src/controllers/product/get.js
+++ src/controllers/product/get.js
@@ -2,21 +2,31 @@
 const Product = require("../../db/schema/product");
 
 const getListProduct = async function (req, res, next) {
-  const { limit = 12, page = null, price = null, search = null } = req.query;
+  const { limit, page, type, price, search, brand } = req.query;
 
-  const count = await Product.count();
+  const query = () => {
+    const findBrand = brand ? { brand: { $in: brand?.split("-") } } : undefined;
+    const findType = type ? { type: { $in: type?.split("-") } } : undefined;
+    const findName = search
+      ? { name: { $regex: search, $options: "i" } }
+      : undefined;
+    const findPrice = price
+      ? {
+          price: {
+            $gte: Number(price?.split("-")[0]),
+            $lte: Number(price?.split("-")[1]),
+          },
+        }
+      : undefined;
 
-  const brand = req.query.brand?.split("-") || null;
-  const type = req.query.type?.split("-") || null;
-  const min = Number(price?.split("-")[0]);
-  const max = Number(price?.split("-")[1]);
+    return { ...findBrand, ...findType, ...findName, ...findPrice };
+  };
 
-  const products = await Product.find(type && { type: { $in: type } })
-    .find(brand && { brand: { $in: brand } })
-    .find(price && { price: { $gte: min, $lte: max } })
-    .find(search && { name: { $regex: search, $options: "i" } })
-    .skip(page && limit * (page - 1))
+  const products = await Product.find(query())
+    .skip(page ? limit * (page - 1) : null)
     .limit(limit);
+
+  const count = await Product.countDocuments(query());
 
   res.json({ products, count });
 };

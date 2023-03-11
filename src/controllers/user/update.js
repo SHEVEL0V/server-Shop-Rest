23461ad3e351f9d77305ad7d1212eed7dcc6e1d9
Андрій Шевельов <ashevelevmti@gmail.com>
2023-03-11 22:51:18 +0200

    fix

--- src/controllers/user/update.js
+++ src/controllers/user/update.js
@@ -1,4 +1,5 @@
 /** @format */
+const bcrypt = require("bcrypt");
 const User = require("../../db/schema/user");
 const { uploadFile } = require("../../services/upload");
 const makeResponseAuth = require("../../services/user/makeRes");
@@ -6,15 +7,21 @@ const makeResponseAuth = require("../../services/user/makeRes");
 const updateUser = async function (req, res, next) {
   const { path, filename } = req.file || {};
   const { id } = req.user;
+  const { body } = req;
 
-  let avatarURL = req.body.picture;
+  let avatarURL = body.picture;
 
   if (path) {
     const { mediaLink } = await uploadFile(path, filename);
     avatarURL = mediaLink;
   }
+
+  const password = body.password
+    ? { password: await bcrypt.hash(body.password, 10) }
+    : undefined;
+
   await User.findByIdAndUpdate(id, {
-    $set: { ...req.body, avatarURL },
+    $set: { ...body, avatarURL, ...password },
   });
 
   const user = await User.findById(id);

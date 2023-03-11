13f1c113a1151325ccbf7060a9c6321eaa558588
Андрій Шевельов <ashevelevmti@gmail.com>
2023-03-11 21:11:49 +0200

    fix

--- src/controllers/user/register.js
+++ src/controllers/user/register.js
@@ -2,6 +2,7 @@
 const bcrypt = require("bcrypt");
 const makeResponseAuth = require("../../services/user/makeRes");
 const User = require("../../db/schema/user");
+const RequestError = require("../../helpers/error");
 
 //-------------------------------------------------------------
 //-------REGISTER----------------------------------------------
@@ -9,12 +10,18 @@ const User = require("../../db/schema/user");
 const registerUser = async (req, res, next) => {
   const { password, email } = req.body;
 
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw new RequestError(400, "User already exists");
+  }
+
   const passwordBcrypt = await bcrypt.hash(password, 10);
 
-  const user = new User({ ...req.body, password: passwordBcrypt });
+  const newUser = new User({ ...req.body, password: passwordBcrypt });
   await newUser.save();
 
-  return res.json(makeResponseAuth(user));
+  return res.json(makeResponseAuth(newUser));
 };
 
 module.exports = registerUser;
